12b1a255888dcadcc869fd1ee8056a8d1cbe8dde
panji182 <mantra_panji@yahoo.com>
2023-02-26 13:37:37 +0700

    Use GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from 'logo.svg';
 import 'App.scss';
+import { useQuery, gql } from "@apollo/client";
+
+const POKEMON_QUERY = gql`
+  {
+    getAllPokemon {
+      color
+      num
+    }
+  }
+`;
 
 function App() {
+  const { data } = useQuery(POKEMON_QUERY);
+
+  console.log(18, data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test
     </div>
   );
 }
